43869e69dc9d2c273b2311b9f8ca1c00a4e6cb6b
PhantomShift <83253309+PhantomShift@users.noreply.github.com>
2024-12-02 10:12:31 -0500

    Implement day 2 part 2

--- src/day2/src/main.rs
+++ src/day2/src/main.rs
@@ -1,4 +1,23 @@
 
+fn is_safe(s: &[i8]) -> bool {
+    let mut w = s.windows(2);
+    match w.next() {
+        Some(first) if (1..=3).contains(&first[0].abs_diff(first[1])) => {
+            let sign = (first[0] - first[1]).signum();
+            for pair in w {
+                let diff = pair[0] - pair[1];
+                if diff.signum() != sign || !(1..=3).contains(&diff.abs()) {
+                    return false
+                }
+            }
+        }
+        _ => {
+            return false
+        }
+    }
+    true
+}
+
 fn solve_part_one(input: &str) -> usize {
     input.lines()
         .filter(|line| !line.trim().is_empty())
@@ -6,22 +25,7 @@ fn solve_part_one(input: &str) -> usize {
             let v: Vec<_> = line.split_ascii_whitespace()
                 .map(|s| s.parse::<i8>().expect("should only be numbers"))
                 .collect();
-            let mut w = v.windows(2);
-            match w.next() {
-                Some(first) if (1..=3).contains(&first[0].abs_diff(first[1])) => {
-                    let sign = (first[0] - first[1]).signum();
-                    for pair in w {
-                        let diff = pair[0] - pair[1];
-                        if diff.signum() != sign || !(1..=3).contains(&diff.abs()) {
-                            return false
-                        }
-                    }
-                }
-                _ => {
-                    return false
-                }
-            }
-            true
+            is_safe(&v)
         })
         .count()
 }
@@ -32,7 +36,34 @@ fn test_part_one() {
     assert_eq!(2, solve_part_one(input));
 }
 
+fn solve_part_two(input: &str) -> usize {
+    input.lines()
+        .filter(|line| {
+            let v: Vec<_> = line.split_ascii_whitespace()
+                .map(|s| s.parse::<i8>().expect("should only be numbers"))
+                .collect();
+
+            if !is_safe(&v) {
+                // Brute force approach because I'm too lazy to consider
+                // how to solve this cleverly
+                (0..v.len()).any(|i| {
+                    is_safe(&[&v[..i], &v[i+1..]].concat())
+                })
+            } else {
+                true
+            }
+        })
+        .count()
+}
+
+#[test]
+fn test_part_two() {
+    let input = include_str!("test.txt");
+    assert_eq!(4, solve_part_two(input));
+}
+
 fn main() {
     let input = include_str!("input.txt");
     println!("Part 1: {}", solve_part_one(input));
+    println!("Part 2: {}", solve_part_two(input));
 }
